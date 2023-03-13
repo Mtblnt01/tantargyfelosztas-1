cf67d41bd0d98b1c8ab40bf61fb280cf3e473191
Mtblnt01 <126462765+Mtblnt01@users.noreply.github.com>
2023-03-13 11:00:42 +0100

    4feladat kesz

--- tanfel.py
+++ tanfel.py
@@ -32,3 +32,18 @@ def osszegzes(bok):
         osszeg+=elem["oraszam"]
     return osszeg
 print(f"3. Feladat {osszegzes(beosztasok)} a heti össz óraszám az iskolában.")
+"""
+4. feladat
+Kérje be a felhasználótól egy tanár nevét, és írassa ki a képernyőre, hogy hetente hány
+órában tanít!
+"""
+be_tanarnev=input("4. Feladat  Egy tanár neve= ") or "Albatrosz Aladin" #ha nem írsz bele semmit ez lesz az érték
+
+def tanar_oraszamanak_osszegzese(bok,be_nev):
+    osszeg=0
+    for elem in bok:
+        if be_nev==elem["tanar"]:
+            osszeg+=elem["oraszam"]
+    return osszeg
+
+print(f"Egy tanár heti óraszáma: {tanar_oraszamanak_osszegzese(beosztasok,be_tanarnev)}")
